22307c016de6d3d059d006409aa704d21e91e7c0
catatonicmark <104708453+catatonicmark@users.noreply.github.com>
2025-11-06 13:36:56 -0700

    more work on timer.js, now counting up and able to save time if paused. buttons are still wonky. added a css file for styling.

--- test_website/static/js/addRow.js
+++ test_website/static/js/addRow.js
@@ -2,11 +2,14 @@ let rowCounter = 0;
 
 function addRow(id)
 {
+  rowCounter++;
   var table = document.getElementById(id).tBodies[0];
   var newRow = table.rows[1].cloneNode(true);
+  newRow.id = `row_${rowCounter}`;
   for (let i = 0; i < newRow.cells.length; i++) {
     const cell = newRow.cells[i];
     const input = cell.firstElementChild;
+    input.id = `${input.id}${rowCounter}`;
     input.value = "";
     if (input.type === "checkbox") {
       input.checked = false;
--- test_website/static/js/timer.js
+++ test_website/static/js/timer.js
@@ -1,20 +1,49 @@
 // initialize variables
 
 let startTime;
+let stopTime = 0;
+const buttonListener = document.getElementById('tracker');
+let buttonId;
+let buttonNumber;
+let intervalId;
+let timeDiff = 0;
+
+buttonListener.addEventListener('click', function(event) {
+  if (event.target.tagName === 'BUTTON') {
+    buttonId = event.target.id;
+    if (event.target.className === 'start-button') {
+      buttonNumber = buttonId.slice(12);
+      console.log('Opposite buttonId is start-button' + buttonNumber);
+      startStopWatch(startTime, buttonId, buttonNumber, stopTime);
+    }
+
+    if (event.target.className === 'stop-button') {
+      buttonNumber = buttonId.slice(11); 
+      console.log('Opposite buttonId is start-button' + buttonNumber);
+      stopStopWatch(buttonId, buttonNumber, timeDiff);
+    }
+    console.log('A button inside the container was clicked!');
+    console.log('Clicked button ID:', buttonId); // Access the ID
+    console.log('Clicked button text:', event.target.textContent); // Access the text
+  }
+});
 
 
-function startStopWatch(startTime) {
-  document.getElementById('start-button').style.display = 'none'; // Hide start-button
-  document.getElementById('stop-button').style.display = 'block'; // Show stop-button
+function startStopWatch(startTime, buttonId, buttonNumber, stopTime) {
+  document.getElementById(buttonId).style.display = 'none'; // Hide start-button
+  document.getElementById('stop-button' + buttonNumber).style.display = 'block'; // Show stop-button
   
-  startTime = new Day();
+  startTime = new Date();
+  intervalId = setInterval(() => {
+    timeDiff = stopTime + Math.floor((new Date() - startTime) / 1000);
+    document.getElementById('timerText' + buttonNumber).innerText = timeDiff;
+  }, 1000);
+ }
 
-  timeDiff = new Day() - startTime;
-
-  document.getElementById('timerText').innerText = timeDiff;
-}
-
-function stopStopWatch() {
-  document.getElementById('start-button').style.display = 'block'; // Hide start-button
-  document.getElementById('stop-button').style.display = 'none'; // Show stop-button
+function stopStopWatch(buttonId, buttonNumber, timeDiff) {
+  document.getElementById('start-button' + buttonNumber).style.display = 'block'; // Show start-button
+  document.getElementById(buttonId).style.display = 'none'; // Hide stop-button
+  clearInterval(intervalId);
+  
+  stopTime = timeDiff;
 }
