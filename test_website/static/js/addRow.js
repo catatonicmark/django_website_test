c0d2cba74901bfadd18ba2c01db0cf38048e94bf
Tyler Pruitt <tylerpruitt123@gmail.com>
2025-11-07 15:45:17 -0700

    wrapped timer in class so that separate timers could be generated. created template to copy instead of top row to avoid data being copied

--- test_website/static/js/addRow.js
+++ test_website/static/js/addRow.js
@@ -1,23 +1,23 @@
 let rowCounter = 0;
 
-function addRow(id)
+function addRow(templateId)
 {
   rowCounter++;
-  var table = document.getElementById(id).tBodies[0];
-  var newRow = table.rows[1].cloneNode(true);
-  newRow.id = `row_${rowCounter}`;
-  for (let i = 0; i < newRow.cells.length; i++) {
-    const cell = newRow.cells[i];
-    const input = cell.firstElementChild;
-    input.id = `${input.id}${rowCounter}`;
-    input.value = "";
-    if (input.type === "checkbox") {
-      input.checked = false;
+  var template = document.getElementById(templateId).content;
+  var newRow = template.cloneNode(true);
+
+  Array.from(newRow.firstElementChild.children).forEach((child) => {
+    
+    child.firstElementChild.id = child.firstElementChild.id + rowCounter;
+
+    if (child.firstElementChild.type === 'date') {
+      child.firstElementChild.valueAsDate = new Date();
     }
-    if (input.type === "date") {
-      input.valueAsDate = new Date(); 
-    }
-  }
-  table.appendChild(newRow);
-  
+  });
+
+  document.getElementById('tracker').tBodies[0].appendChild(newRow);
 }
+
+
+// initial call of this so first row is automatically populated
+addRow('template-row');
--- test_website/static/js/timer.js
+++ test_website/static/js/timer.js
@@ -1,49 +1,57 @@
-// initialize variables
+// list of all timers
+const timers = {};
 
-let startTime;
-let stopTime = 0;
+// listens for button pushes, creates new timer if one does not exist, otherwise starts/stops the stopwatch 
 const buttonListener = document.getElementById('tracker');
-let buttonId;
-let buttonNumber;
-let intervalId;
-let timeDiff = 0;
-
 buttonListener.addEventListener('click', function(event) {
   if (event.target.tagName === 'BUTTON') {
     buttonId = event.target.id;
     if (event.target.className === 'start-button') {
       buttonNumber = buttonId.slice(12);
-      console.log('Opposite buttonId is start-button' + buttonNumber);
-      startStopWatch(startTime, buttonId, buttonNumber, stopTime);
+      if (!timers[buttonNumber]) {
+        timers[buttonNumber] = new Timer(buttonId, buttonNumber);
+      }
+
+      timers[buttonNumber].startStopWatch();
     }
 
     if (event.target.className === 'stop-button') {
-      buttonNumber = buttonId.slice(11); 
-      console.log('Opposite buttonId is start-button' + buttonNumber);
-      stopStopWatch(buttonId, buttonNumber, timeDiff);
+      buttonNumber = buttonId.slice(11);
+            timers[buttonNumber].stopStopWatch();
     }
-    console.log('A button inside the container was clicked!');
-    console.log('Clicked button ID:', buttonId); // Access the ID
-    console.log('Clicked button text:', event.target.textContent); // Access the text
+    
   }
 });
 
 
-function startStopWatch(startTime, buttonId, buttonNumber, stopTime) {
-  document.getElementById(buttonId).style.display = 'none'; // Hide start-button
-  document.getElementById('stop-button' + buttonNumber).style.display = 'block'; // Show stop-button
-  
-  startTime = new Date();
-  intervalId = setInterval(() => {
-    timeDiff = stopTime + Math.floor((new Date() - startTime) / 1000);
-    document.getElementById('timerText' + buttonNumber).innerText = timeDiff;
-  }, 1000);
- }
+// timer definition
+class Timer{
+  constructor(buttonId, buttonNumber) {
+    this.startTime = new Date();
+    this.stopTime = 0;
+    this.buttonId = buttonId;
+    this.buttonNumber = buttonNumber;
+    this.intervalId;
+    this.timeDiff = 0;
+  }
 
-function stopStopWatch(buttonId, buttonNumber, timeDiff) {
-  document.getElementById('start-button' + buttonNumber).style.display = 'block'; // Show start-button
-  document.getElementById(buttonId).style.display = 'none'; // Hide stop-button
-  clearInterval(intervalId);
-  
-  stopTime = timeDiff;
+ // starts the stop watch 
+  startStopWatch() {
+    document.getElementById(this.buttonId).style.display = 'none'; // Hide start-button
+    document.getElementById('stop-button' + this.buttonNumber).style.display = 'block'; // Show stop-button
+    
+    this.intervalId = setInterval(() => {
+      this.timeDiff = this.stopTime + Math.floor((new Date() - this.startTime) / 1000);
+      document.getElementById('timerText' + this.buttonNumber).innerText = this.timeDiff;
+    }, 1000);
+   }
+// stops the stopwatch 
+  stopStopWatch() {
+    document.getElementById(this.buttonId).style.display = 'none'; // Hide stop-button
+    document.getElementById('start-button' + this.buttonNumber).style.display = 'block'; // Show start-button
+    clearInterval(this.intervalId);
+    
+    this.stopTime = this.timeDiff;
+  }
+
 }
