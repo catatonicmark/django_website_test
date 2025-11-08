31eca9e626e7541f638c3d3a6dceda939c33751b
Tyler Pruitt <tylerpruitt123@gmail.com>
2025-11-07 19:10:06 -0700

    got timer updating at millisecond and consistently updating on each separate instance of timer object

--- test_website/static/js/timer.js
+++ test_website/static/js/timer.js
@@ -32,6 +32,7 @@ class Timer{
     this.buttonNumber = buttonNumber;
     this.intervalId = null;
     this.elapsedTime = 0;
+    this.timerText = "00:00:000"
   }
 
  // starts the stop watch 
@@ -39,12 +40,13 @@ class Timer{
     document.getElementById(this.buttonId).style.display = 'none'; // Hide start-button
     document.getElementById('stop-button' + this.buttonNumber).style.display = 'block'; // Show stop-button
 
-    this.startTime = new Date() - this.elapsedTime * 1000;
+    this.startTime = new Date() - this.elapsedTime;
 
+    // set stopStopWatch text to continuosly update until stop button pushed
     this.intervalId = setInterval(() => {
-      this.elapsedTime = Math.floor((new Date() - this.startTime) / 1000);
-      document.getElementById('timerText' + this.buttonNumber).innerText = this.elapsedTime;
-    }, 1000);
+      this.elapsedTime = (new Date() - this.startTime);
+      document.getElementById('timerText' + this.buttonNumber).innerText = this.parseTimeToText();
+    }, 17);
    }
 // stops the stopwatch 
   stopStopWatch() {
@@ -53,5 +55,39 @@ class Timer{
     clearInterval(this.intervalId);
     this.intervalId = null;
   }
+  
+// timer parsing
+  parseTimeToText () {
+    // initiliaze all variables
+    var milliseconds = this.elapsedTime;
+    var seconds = Math.floor(this.elapsedTime / 1000);
+    var minutes = Math.floor(seconds / 60);
+    var hours = Math.floor(minutes / 60);
+    var days = hours / 24;
+    var weeks = days / 7;
 
+    // put into array so they can be iterated over
+    const increments = [
+      seconds,
+      minutes,
+      hours,
+      days,
+      weeks
+    ]
+
+    // make sure that the numbers move from one section to the next on the timer
+    milliseconds = milliseconds - seconds * 1000;
+    seconds = seconds - minutes * 60;
+    minutes = minutes - hours * 60;
+    hours = hours - days * 24;
+
+    // prepend a 0 if the number is below 10 to maintain consistent display
+    increments.forEach((number) => {
+      if (number < 10) {
+        number = "0" + number;
+      }
+    }); 
+
+    return hours + ":" + minutes + ":" + seconds + ":" + milliseconds;
+  }
 }
