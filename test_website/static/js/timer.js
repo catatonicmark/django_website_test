d07022ca416a33016b44f91502e208105a5f5800
Tyler Pruitt <tylerpruitt123@gmail.com>
2025-11-08 10:43:37 -0700

    updated & refactored timer function. added feature that it maintains consistent display that units always look like 00:00:000. it will update to 00:00:00, with seconds replacing milliseconds and hours being in the first column when it reaches.

--- test_website/static/js/timer.js
+++ test_website/static/js/timer.js
@@ -4,90 +4,80 @@ const timers = {};
 // listens for button pushes, creates new timer if one does not exist, otherwise starts/stops the stopwatch 
 const buttonListener = document.getElementById('tracker');
 buttonListener.addEventListener('click', function(event) {
-  if (event.target.tagName === 'BUTTON') {
-    buttonId = event.target.id;
-    if (event.target.className === 'start-button') {
-      buttonNumber = buttonId.slice(12);
-      if (!timers[buttonNumber]) {
-        timers[buttonNumber] = new Timer(buttonId, buttonNumber);
-      }
+  buttonId = event.target.id;
 
-      timers[buttonNumber].startStopWatch();
-    }
+  if (event.target.className === 'start-button') { 
+    buttonNumber = buttonId.slice(12);
 
-    if (event.target.className === 'stop-button') {
-      buttonNumber = buttonId.slice(11);
-      timers[buttonNumber].stopStopWatch();
+    if (!timers[buttonNumber]) {
+      timers[buttonNumber] = new Timer(buttonNumber);
     }
-    
+    timers[buttonNumber].startStopWatch();
   }
+
+  if (event.target.className === 'stop-button') {
+    buttonNumber = buttonId.slice(11);
+    timers[buttonNumber].stopStopWatch();
+  }  
 });
 
 
 // timer definition
 class Timer{
-  constructor(buttonId, buttonNumber) {
+  constructor(buttonNumber) {
     this.startTime = null;
-    this.buttonId = buttonId;
+    this.startButtonId = 'start-button' + buttonNumber;
+    this.stopButtonId = 'stop-button' + buttonNumber;
     this.buttonNumber = buttonNumber;
     this.intervalId = null;
     this.elapsedTime = 0;
-    this.timerText = "00:00:000"
+    this.timerText = null;
   }
 
- // starts the stop watch 
+ // starts the StopWatch  
   startStopWatch() {
-    document.getElementById(this.buttonId).style.display = 'none'; // Hide start-button
-    document.getElementById('stop-button' + this.buttonNumber).style.display = 'block'; // Show stop-button
-
+    document.getElementById(this.startButtonId).style.display = 'none'; // Hide start-button
+    document.getElementById(this.stopButtonId).style.display = 'block'; // Show stop-button
     this.startTime = new Date() - this.elapsedTime;
 
-    // set stopStopWatch text to continuosly update until stop button pushed
+    // set StopWatch text to continuosly update until stop button pushed
     this.intervalId = setInterval(() => {
       this.elapsedTime = (new Date() - this.startTime);
-      document.getElementById('timerText' + this.buttonNumber).innerText = this.parseTimeToText();
+      this.parseTimeToText();
+      document.getElementById('timerText' + this.buttonNumber).innerText = this.timerText;
     }, 17);
    }
-// stops the stopwatch 
+// stops the stopwatch
   stopStopWatch() {
-    document.getElementById(this.buttonId).style.display = 'none'; // Hide stop-button
-    document.getElementById('start-button' + this.buttonNumber).style.display = 'block'; // Show start-button
     clearInterval(this.intervalId);
     this.intervalId = null;
+    document.getElementById(this.stopButtonId).style.display = 'none'; // Hide stop-button
+    document.getElementById(this.startButtonId).style.display = 'block'; // Show start-button
   }
   
 // timer parsing
-  parseTimeToText () {
-    // initiliaze all variables
-    var milliseconds = this.elapsedTime;
-    var seconds = Math.floor(this.elapsedTime / 1000);
-    var minutes = Math.floor(seconds / 60);
-    var hours = Math.floor(minutes / 60);
-    var days = hours / 24;
-    var weeks = days / 7;
-
-    // put into array so they can be iterated over
-    const increments = [
-      seconds,
-      minutes,
-      hours,
-      days,
-      weeks
-    ]
-
-    // make sure that the numbers move from one section to the next on the timer
-    milliseconds = milliseconds - seconds * 1000;
-    seconds = seconds - minutes * 60;
-    minutes = minutes - hours * 60;
-    hours = hours - days * 24;
+  parseTimeToText() {
+    // initiliaze all time variables
+    let milliseconds = this.elapsedTime % 1000;
+    let seconds = Math.floor(this.elapsedTime / 1000) % 60;
+    let minutes = Math.floor(this.elapsedTime / (1000 * 60)) % 60;
+    let hours = Math.floor(this.elapsedTime / (1000 * 60 * 60)) % 24;
+    let days = Math.floor(this.elapsedTime / (1000 * 60 * 60 * 24)) % 7;
+    let weeks = Math.floor(this.elapsedTime / (1000 * 60 * 60 * 24 * 7)) % 4;
 
     // prepend a 0 if the number is below 10 to maintain consistent display
-    increments.forEach((number) => {
-      if (number < 10) {
-        number = "0" + number;
-      }
-    }); 
-
-    return hours + ":" + minutes + ":" + seconds + ":" + milliseconds;
+    // this is a ternary operator is javascript, which is basically shorthand if-else statement
+    // can be read as if num < 10 return "0" + num else return num
+    function timeToTextHelper(num) {
+      return num < 10 ? "0" + num : num;
+    }
+    
+    // format time so that their is a consistent display
+    if (hours > 1) {
+      this.timerText = `${timeToTextHelper(hours)}:${timeToTextHelper(minutes)}:${timeToTextHelper(seconds)}`;
+    } else {
+      this.timerText = `${timeToTextHelper(minutes)}:${timeToTextHelper(seconds)}:${timeToTextHelper(milliseconds)}`;
+    }
+    
   }
 }
