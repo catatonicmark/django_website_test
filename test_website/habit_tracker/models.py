e080c88addcf2e8498ea3d5ff3d9dccd6a7060e9
catatonicmark <104708453+catatonicmark@users.noreply.github.com>
2025-11-10 09:28:27 -0700

    developing model and modelformset for habit data

--- test_website/habit_tracker/models.py
+++ test_website/habit_tracker/models.py
@@ -1,8 +1,14 @@
 from django.db import models
+from django.forms import ModelForm
 
 # Create your models here.
-class habit(models.Model):
+class habitModel(models.Model):
     completed = models.BooleanField()
     habit_name = models.CharField(max_length=100, on_delete=models.CASCADE)
     date = models.DateField()
     duration = models.DurationField(blank=True)
+
+class habitForm(ModelForm):
+    class Meta:
+        model = habitModel
+        fields = ["completed", "habit_name", "date", "duration"]
--- test_website/habit_tracker/views.py
+++ test_website/habit_tracker/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render, redirect
 from django.forms import formset_factory
+from django.forms import modelformset_factory
 from .forms import habitForm
+from .models import habitModel
 
 
 # Create your views here.
 
 def get_habit(request):
-    habitFormSet = formset_factory(habitForm)
+    habitFormSet = modelformset_factory(habitModel)
     if request.method == 'POST':
         formset = habitFormSet(request.POST, request.FILES) 
         if formset.is_valid():
