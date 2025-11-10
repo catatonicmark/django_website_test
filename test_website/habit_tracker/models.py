551d160ae64b5f8b914d727d947c34780e6fc173
Tyler Pruitt <tylerpruitt123@gmail.com>
2025-11-10 07:09:41 -0700

    created model for habit database

--- test_website/habit_tracker/models.py
+++ test_website/habit_tracker/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class habit(models.Model):
+    completed = models.BooleanField()
+    habit_name = models.CharField(max_length=100, on_delete=models.CASCADE)
+    date = models.DateField()
+    duration = models.DurationField(blank=True)
